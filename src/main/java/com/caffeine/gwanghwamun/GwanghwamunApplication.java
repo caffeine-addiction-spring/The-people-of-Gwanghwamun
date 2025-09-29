af5af2bc7b8a36ad768eaa723d52f23ef1143166
seolbin01 <se01bin@suwon.ac.kr>
2025-09-29 23:38:41 +0900

    init commit

---
+++ src/main/java/com/caffeine/gwanghwamun/GwanghwamunApplication.java
@@ -0,0 +1,13 @@
+package com.caffeine.gwanghwamun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwanghwamunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GwanghwamunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/caffeine/gwanghwamun/GwanghwamunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caffeine.gwanghwamun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwanghwamunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
