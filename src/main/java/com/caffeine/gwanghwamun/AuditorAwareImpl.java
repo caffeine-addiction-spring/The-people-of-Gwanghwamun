7d190a5748a0e9de215c540c4a980cbe9a5caf02
minju26 <mjalswn26@gmail.com>
2025-09-30 22:29:47 +0900

    refactor: AuditorAware 인증 로직 주석 처리
    - TODO: Spring Security 구현 후 재활성화
    - 현재는 "Test" 고정값 사용

--- src/main/java/com/caffeine/gwanghwamun/AuditorAwareImpl.java
+++ src/main/java/com/caffeine/gwanghwamun/AuditorAwareImpl.java
@@ -14,15 +14,18 @@ public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
     @NonNull
     public Optional<String> getCurrentAuditor() {
-        Authentication authentication = SecurityContextHolder
-                .getContext()
-                .getAuthentication();
+        // TODO: Spring Security 구현 후 주석 해제
+//        Authentication authentication = SecurityContextHolder
+//                .getContext()
+//                .getAuthentication();
+//
+//        if (authentication == null || !authentication.isAuthenticated()) {
+//            return Optional.empty();
+//        }
+//
+//        return Optional.of(authentication.getName());
 
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return Optional.empty();
-        }
-
-        return Optional.of(authentication.getName());
-//        return Optional.of("Test");
+        // 임시: Spring Security 미구현으로 고정값 사용
+        return Optional.of("Test");
     }
 }
