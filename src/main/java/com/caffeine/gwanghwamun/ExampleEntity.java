a3c93f5fdd98f32b04491a6258be95c70e32a9ba
minju26 <mjalswn26@gmail.com>
2025-09-30 22:13:47 +0900

    test: JPA Auditing 동작 확인을 위한 테스트 설정

---
+++ src/main/java/com/caffeine/gwanghwamun/ExampleEntity.java
@@ -0,0 +1,24 @@
+package com.caffeine.gwanghwamun;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ExampleEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    public ExampleEntity(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/caffeine/gwanghwamun/TestRepository.java
@@ -0,0 +1,6 @@
+package com.caffeine.gwanghwamun;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestRepository extends JpaRepository<ExampleEntity, Long> {
+}
