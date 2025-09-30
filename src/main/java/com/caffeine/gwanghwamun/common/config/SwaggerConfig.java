097efd8dce7c76bb34b4cfe9bb74120ffeb10036
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2025-09-30 21:59:50 +0900

    chore : spotless 적용

--- src/main/java/com/caffeine/gwanghwamun/common/config/SwaggerConfig.java
+++ src/main/java/com/caffeine/gwanghwamun/common/config/SwaggerConfig.java
@@ -2,21 +2,15 @@ package com.caffeine.gwanghwamun.common.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("광화문의 민족 API")
-                        .description("배달 주문 관리 플랫폼 API 문서")
-                        .version("v1.0.0")
-                        );
-    }
-
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("광화문의 민족 API").description("배달 주문 관리 플랫폼 API 문서").version("v1.0.0"));
+	}
 }
