88ae234b8ed71b11cf8f66eafda53e24430d7ee7
minju26 <mjalswn26@gmail.com>
2025-10-02 14:38:37 +0900

    del: auditing test 관련 파일 삭제

--- src/main/java/com/caffeine/gwanghwamun/domain/test/repository/TestRepository.java
+++
@@ -1,6 +0,0 @@
-package com.caffeine.gwanghwamun.domain.test.repository;
-
-import com.caffeine.gwanghwamun.domain.test.entity.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity, Long> {}
