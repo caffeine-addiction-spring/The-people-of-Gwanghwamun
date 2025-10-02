4a1d1b55aab40f4bb849f6bf95753df8f226c121
minju26 <mjalswn26@gmail.com>
2025-10-02 15:45:50 +0900

    refactor: 공통 응답 적용

--- src/main/java/com/caffeine/gwanghwamun/domain/user/controller/UserController.java
+++ src/main/java/com/caffeine/gwanghwamun/domain/user/controller/UserController.java
@@ -1,19 +1,17 @@
 package com.caffeine.gwanghwamun.domain.user.controller;
 
+import com.caffeine.gwanghwamun.common.response.ApiResponse;
+import com.caffeine.gwanghwamun.common.response.ResponseUtil;
+import com.caffeine.gwanghwamun.common.success.SuccessCode;
 import com.caffeine.gwanghwamun.domain.user.dto.SignupReqDTO;
 import com.caffeine.gwanghwamun.domain.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,27 +25,8 @@ public class UserController {
 
 	@PostMapping("/auth/signup")
 	@Operation(summary = "회원가입 API")
-	@Parameters({
-		@Parameter(
-				name = "email",
-				description = "회원 이메일",
-				content = @Content(schema = @Schema(implementation = String.class))),
-		@Parameter(
-				name = "password",
-				description = "회원 비밀번호",
-				content = @Content(schema = @Schema(implementation = String.class))),
-		@Parameter(
-				name = "name",
-				description = "회원 이름",
-				content = @Content(schema = @Schema(implementation = String.class))),
-		@Parameter(
-				name = "phone",
-				description = "회원 전화번호",
-				content = @Content(schema = @Schema(implementation = String.class)))
-	})
-	@ApiResponse(responseCode = "201", description = "회원가입 성공")
-	public ResponseEntity<Void> signup(@Valid SignupReqDTO requestDto) {
+	public ResponseEntity<ApiResponse<Void>> signup(@RequestBody @Valid SignupReqDTO requestDto) {
 		userService.signUp(requestDto);
-		return new ResponseEntity<>(HttpStatus.CREATED);
+		return ResponseUtil.successResponse(SuccessCode.USER_SAVE_SUCCESS);
 	}
 }
