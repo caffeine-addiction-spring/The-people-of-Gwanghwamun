bab69e2a09a5a07cba6164bd32e1d8fe2c1479af
minju26 <mjalswn26@gmail.com>
2025-10-02 16:08:01 +0900

    docs: Swagger API 문서화 내용 추가

--- src/main/java/com/caffeine/gwanghwamun/domain/user/controller/UserController.java
+++ src/main/java/com/caffeine/gwanghwamun/domain/user/controller/UserController.java
@@ -6,6 +6,8 @@ import com.caffeine.gwanghwamun.common.success.SuccessCode;
 import com.caffeine.gwanghwamun.domain.user.dto.SignupReqDTO;
 import com.caffeine.gwanghwamun.domain.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,7 +26,19 @@ public class UserController {
 	private final UserService userService;
 
 	@PostMapping("/auth/signup")
-	@Operation(summary = "회원가입 API")
+	@Operation(
+			summary = "회원가입 API",
+			requestBody =
+					@io.swagger.v3.oas.annotations.parameters.RequestBody(
+							description = "회원가입 요청 정보",
+							required = true,
+							content =
+									@Content(
+											mediaType = "application/json",
+											schema = @Schema(implementation = SignupReqDTO.class))))
+	@io.swagger.v3.oas.annotations.responses.ApiResponse(
+			responseCode = "201",
+			description = "회원가입 성공")
 	public ResponseEntity<ApiResponse<Void>> signup(@RequestBody @Valid SignupReqDTO requestDto) {
 		userService.signUp(requestDto);
 		return ResponseUtil.successResponse(SuccessCode.USER_SAVE_SUCCESS);
