e872650bba245e905ce0074a91707cff1cf2e714
SeongYeon Han <cicle1999@gmail.com>
2025-10-08 19:27:02 +0900

    feat: 가게 ErrorCode 추가

--- src/main/java/com/caffeine/gwanghwamun/common/exception/ErrorCode.java
+++ src/main/java/com/caffeine/gwanghwamun/common/exception/ErrorCode.java
@@ -24,7 +24,10 @@ public enum ErrorCode {
 
 	// 메뉴 (menu)
 	MENU_NOT_FOUND(HttpStatus.NOT_FOUND, "메뉴를 찾을 수 없습니다."),
-	MENU_STORE_MISMATCH(HttpStatus.FORBIDDEN, "해당 가게의 메뉴가 아닙니다.");
+	MENU_STORE_MISMATCH(HttpStatus.FORBIDDEN, "해당 가게의 메뉴가 아닙니다."),
+
+	// 가게 (store)
+	STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 가게를 찾을 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
