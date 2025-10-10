ce97adfa6eb986197833dd48e72e1f7b731b7881
Daae-Kim <wlsemrdl1212@naver.com>
2025-10-10 13:00:10 +0900

    feat : review, reply 엔티티 생성

---
+++ src/main/java/com/caffeine/gwanghwamun/domain/review/entity/ReviewReply.java
@@ -0,0 +1,40 @@
+package com.caffeine.gwanghwamun.domain.review.entity;
+
+import com.caffeine.gwanghwamun.domain.BaseEntity;
+import com.fasterxml.jackson.databind.ser.Serializers;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "p_review_reply")
+public class ReviewReply extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "reply_id")
+    private Long replyId;
+
+    @Column(name = "review_id", nullable = false)
+    private Long reviewId;
+
+    @Column(name = "owner_id", nullable = false)
+    private Long ownerId;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public ReviewReply(Long reviewId, Long ownerId, String content){
+        this.reviewId = reviewId;
+        this.ownerId = ownerId;
+        this.content = content;
+    }
+
+    public void updateContent(String content){
+        this.content = content;
+    }
+}
