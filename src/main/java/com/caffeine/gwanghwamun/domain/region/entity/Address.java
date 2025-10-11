cae5496cb5933db3817c278f7d59f93edaba0ef9
SeongYeon Han <cicle1999@gmail.com>
2025-10-11 22:06:20 +0900

    feat: 지역, 배달가능지역 entity 생성

---
+++ src/main/java/com/caffeine/gwanghwamun/domain/region/entity/Address.java
@@ -0,0 +1,23 @@
+package com.caffeine.gwanghwamun.domain.region.entity;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "p_addresses")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID addressId;
+
+	@Column(nullable = false)
+	private String name;
+}
