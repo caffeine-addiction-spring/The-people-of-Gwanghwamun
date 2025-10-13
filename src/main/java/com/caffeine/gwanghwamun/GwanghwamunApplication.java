aca6271a0ff7331d79fca600dd0892254077cfa4
seolbin01 <se01bin@suwon.ac.kr>
2025-10-13 10:44:34 +0900

    chore: 로컬 file 경로 제거

--- src/main/java/com/caffeine/gwanghwamun/GwanghwamunApplication.java
+++ src/main/java/com/caffeine/gwanghwamun/GwanghwamunApplication.java
@@ -1,12 +1,9 @@
 package com.caffeine.gwanghwamun;
 
-import com.caffeine.gwanghwamun.common.domain.FileProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(FileProperties.class)
 public class GwanghwamunApplication {
 
 	public static void main(String[] args) {
