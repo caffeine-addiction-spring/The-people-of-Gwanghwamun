aff68b6372b403776cab08372554249ba9dba6ad
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2025-10-13 12:26:40 +0900

    feat : 리뷰 응답 DTO 추가

---
+++ src/main/java/com/caffeine/gwanghwamun/domain/review/dto/ReviewResDTO.java
@@ -0,0 +1,23 @@
+package com.caffeine.gwanghwamun.domain.review.dto;
+
+import com.caffeine.gwanghwamun.domain.review.entity.Review;
+import java.util.UUID;
+
+public record ReviewResDTO(
+        UUID reviewId,
+        String content,
+        Long rating,
+        ReviewReplyResDTO reply
+        ) {
+
+        public static ReviewResDTO from(Review review) {
+                return new ReviewResDTO(
+                        review.getReviewId(),
+                        review.getContent(),
+                        review.getRating(),
+                        review.getReply() != null
+                        ? ReviewReplyResDTO.from(review.getReply())
+                                : null
+                );
+        }
+}
