437c6b65560ea14c394fa8c675856eac2241f920
seolbin01 <se01bin@suwon.ac.kr>
2025-10-13 10:18:03 +0900

    fix: 파일 업로드 API에 multipart/form-data consume 타입 명시

--- src/main/java/com/caffeine/gwanghwamun/domain/file/controller/FileController.java
+++ src/main/java/com/caffeine/gwanghwamun/domain/file/controller/FileController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,7 @@ public class FileController {
 
 	private final FileService fileService;
 
-	@PostMapping("/upload")
+	@PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public List<FileInfoResDTO> upload(
 			@RequestPart(name = "file", required = false) MultipartFile[] files,
 			@Valid FileUploadReqDTO requestDTO,
