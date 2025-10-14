057528d6482242bb192e4fd82d5399d3c53a0e70
Daae-Kim <wlsemrdl1212@naver.com>
2025-10-14 15:13:37 +0900

    feat : ReviewRepository 메서드 생성

--- src/main/java/com/caffeine/gwanghwamun/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/caffeine/gwanghwamun/domain/review/repository/ReviewRepository.java
@@ -1,9 +1,21 @@
 package com.caffeine.gwanghwamun.domain.review.repository;
 
 import com.caffeine.gwanghwamun.domain.review.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
+    @EntityGraph(attributePaths = {"reply"})
+    Page<Review> findByStoreId(UUID storeId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"reply"})
+    @Query("select r from Review r where r.reviewId = :id")
+    Optional<Review> findByIdWithReply(@Param("id") UUID id);
 }
