42712679752abd854bc0d00da1c0b5cf44744115
seolbin01 <se01bin@suwon.ac.kr>
2025-10-14 10:27:58 +0900

    refactor: 컨트롤러에 공통 응답 유틸 적용

--- src/main/java/com/caffeine/gwanghwamun/domain/file/controller/FileController.java
+++ src/main/java/com/caffeine/gwanghwamun/domain/file/controller/FileController.java
@@ -1,5 +1,9 @@
 package com.caffeine.gwanghwamun.domain.file.controller;
 
+import com.caffeine.gwanghwamun.common.exception.ErrorCode;
+import com.caffeine.gwanghwamun.common.response.ApiResponse;
+import com.caffeine.gwanghwamun.common.response.ResponseUtil;
+import com.caffeine.gwanghwamun.common.success.SuccessCode;
 import com.caffeine.gwanghwamun.domain.file.dto.FileInfoResDTO;
 import com.caffeine.gwanghwamun.domain.file.dto.FileUploadReqDTO;
 import com.caffeine.gwanghwamun.domain.file.service.FileService;
@@ -10,6 +14,7 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,52 +29,51 @@ public class FileController {
 
 	@Operation(summary = "파일 업로드")
 	@PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public List<FileInfoResDTO> upload(
+	public ResponseEntity<ApiResponse<List<FileInfoResDTO>>> upload(
 			@RequestPart(name = "file", required = false) MultipartFile[] files,
 			@Valid @RequestPart(name = "requestDTO") FileUploadReqDTO requestDTO,
 			Errors errors) {
 		if (errors.hasErrors()) {
-
-			List<FileInfoResDTO> items = fileService.upload(files, requestDTO);
-			return items;
+			return ResponseUtil.failureResponse(ErrorCode.VALIDATION_ERROR);
 		}
 
-		return null;
+		List<FileInfoResDTO> items = fileService.upload(files, requestDTO);
+		return ResponseUtil.successResponse(SuccessCode.FILE_UPLOAD_SUCCESS, items);
 	}
 
 	@Operation(summary = "파일 정보 조회")
 	@GetMapping("/{uuid}")
-	public FileInfoResDTO getFile(@PathVariable String uuid) {
+	public ResponseEntity<ApiResponse<FileInfoResDTO>> getFile(@PathVariable String uuid) {
 		FileInfoResDTO item = fileService.get(UUID.fromString(uuid));
-		return item;
+		return ResponseUtil.successResponse(SuccessCode.FILE_READ_SUCCESS, item);
 	}
 
 	@Operation(summary = "파일 정보 그룹 조회")
 	@GetMapping({"/list/{gid}", "/list/{gid}/{location}"})
-	public List<FileInfoResDTO> getFileList(
+	public ResponseEntity<ApiResponse<List<FileInfoResDTO>>> getFileList(
 			@PathVariable("gid") String gid,
 			@PathVariable(name = "location", required = false) String location) {
 
 		List<FileInfoResDTO> items = fileService.getList(gid, location);
 
-		return items;
+		return ResponseUtil.successResponse(SuccessCode.FILE_READ_SUCCESS, items);
 	}
 
 	@Operation(summary = "파일 정보 삭제")
 	@DeleteMapping("/{uuid}")
-	public FileInfoResDTO deleteFile(@PathVariable String uuid) {
-		FileInfoResDTO item = fileService.deleteFile(UUID.fromString(uuid));
+	public ResponseEntity<ApiResponse<Void>> deleteFile(@PathVariable String uuid) {
+		fileService.deleteFile(UUID.fromString(uuid));
 
-		return item;
+		return ResponseUtil.successResponse(SuccessCode.FILE_DELETE_SUCCESS);
 	}
 
 	@Operation(summary = "파일 정보 그룹 삭제")
 	@DeleteMapping({"/deletes/{gid}", "/deletes/{gid}/{location}"})
-	public List<FileInfoResDTO> deleteFileList(
+	public ResponseEntity<ApiResponse<Void>> deleteFileList(
 			@PathVariable("gid") String gid,
 			@PathVariable(name = "location", required = false) String location) {
-		List<FileInfoResDTO> items = fileService.deleteFiles(gid, location);
+		fileService.deleteFiles(gid, location);
 
-		return items;
+		return ResponseUtil.successResponse(SuccessCode.FILE_DELETE_SUCCESS);
 	}
 }
