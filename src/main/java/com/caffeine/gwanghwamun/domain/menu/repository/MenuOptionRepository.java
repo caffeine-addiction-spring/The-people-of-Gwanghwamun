2d62a7e1206d6dd8d667c4093391f66e73f05a6d
taeaeaeae <haenimhana@gmail.com>
2025-10-15 07:35:05 +0900

    fix: 충돌 해결 후 빠진 import 추가

--- src/main/java/com/caffeine/gwanghwamun/domain/menu/repository/MenuOptionRepository.java
+++ src/main/java/com/caffeine/gwanghwamun/domain/menu/repository/MenuOptionRepository.java
@@ -3,50 +3,52 @@ package com.caffeine.gwanghwamun.domain.menu.repository;
 import com.caffeine.gwanghwamun.domain.menu.entity.MenuOption;
 import com.caffeine.gwanghwamun.domain.menu.entity.QMenuOption;
 import com.querydsl.core.BooleanBuilder;
-import java.util.Optional;
-import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.util.StringUtils;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface MenuOptionRepository
-		extends JpaRepository<MenuOption, UUID>, QuerydslPredicateExecutor<MenuOption> {
+    extends JpaRepository<MenuOption, UUID>, QuerydslPredicateExecutor<MenuOption> {
 
-	default Optional<MenuOption> findByIdAndNotDeleted(UUID optionId) {
-		BooleanBuilder builder = new BooleanBuilder();
-		QMenuOption menuOption = QMenuOption.menuOption;
-		builder.and(menuOption.menuOptionId.eq(optionId)).and(menuOption.deletedAt.isNull());
-		return findOne(builder);
-	}
+  default Optional<MenuOption> findByIdAndNotDeleted(UUID optionId) {
+    BooleanBuilder builder = new BooleanBuilder();
+    QMenuOption menuOption = QMenuOption.menuOption;
+    builder.and(menuOption.menuOptionId.eq(optionId)).and(menuOption.deletedAt.isNull());
+    return findOne(builder);
+  }
 
-	default Page<MenuOption> findPublicListByMenuId(UUID menuId, Pageable pageable) {
-		BooleanBuilder builder = new BooleanBuilder();
-		QMenuOption menuOption = QMenuOption.menuOption;
-		builder
-				.and(menuOption.menuOptionId.eq(menuId))
-				.and(menuOption.deletedAt.isNull())
-				.and(menuOption.isHidden.eq(false));
-		return findAll(builder, pageable);
-	}
+  default Page<MenuOption> findPublicListByMenuId(UUID menuId, Pageable pageable) {
+    BooleanBuilder builder = new BooleanBuilder();
+    QMenuOption menuOption = QMenuOption.menuOption;
+    builder
+        .and(menuOption.menuOptionId.eq(menuId))
+        .and(menuOption.deletedAt.isNull())
+        .and(menuOption.isHidden.eq(false));
+    return findAll(builder, pageable);
+  }
 
-	default Page<MenuOption> searchOptions(
-			UUID menuId, Boolean includeHidden, Boolean soldOut, String optionName, Pageable pageable) {
-		BooleanBuilder andBuilder = new BooleanBuilder();
-		QMenuOption menuOption = QMenuOption.menuOption;
-		andBuilder
-				.and(menuOption.menuId.eq(menuId))
-				.and(menuOption.isSoldOut.eq(soldOut != null && soldOut))
-				.and(menuOption.deletedAt.isNull());
-		if (StringUtils.hasText(optionName)) {
-			andBuilder.and(menuOption.optionName.containsIgnoreCase(optionName));
-		}
+  default Page<MenuOption> searchOptions(
+      UUID menuId, Boolean includeHidden, Boolean soldOut, String optionName, Pageable pageable) {
+    BooleanBuilder andBuilder = new BooleanBuilder();
+    QMenuOption menuOption = QMenuOption.menuOption;
+    andBuilder
+        .and(menuOption.menuId.eq(menuId))
+        .and(menuOption.isSoldOut.eq(soldOut != null && soldOut))
+        .and(menuOption.deletedAt.isNull());
+    if (StringUtils.hasText(optionName)) {
+      andBuilder.and(menuOption.optionName.containsIgnoreCase(optionName));
+    }
 
-		if (includeHidden != null && includeHidden) andBuilder.and(menuOption.isHidden.eq(true));
+    if (includeHidden != null && includeHidden) andBuilder.and(menuOption.isHidden.eq(true));
 
-		return findAll(andBuilder, pageable);
-	}
-      
+    return findAll(andBuilder, pageable);
+  }
+
   List<MenuOption> findAllByMenuOptionIdInAndMenuId(List<UUID> menuOptionIdList, UUID menuId);
 }
