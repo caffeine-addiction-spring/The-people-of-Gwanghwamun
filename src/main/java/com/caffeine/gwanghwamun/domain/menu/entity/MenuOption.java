0967ebb8391053e03405bb8d9cd06f6c7069c33a
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-10-15 11:25:00 +0900

    refactor: content 필드 타입 명시

--- src/main/java/com/caffeine/gwanghwamun/domain/menu/entity/MenuOption.java
+++ src/main/java/com/caffeine/gwanghwamun/domain/menu/entity/MenuOption.java
@@ -2,12 +2,13 @@ package com.caffeine.gwanghwamun.domain.menu.entity;
 
 import com.caffeine.gwanghwamun.domain.BaseEntity;
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
-import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "p_menu_option")
@@ -31,7 +32,8 @@ public class MenuOption extends BaseEntity {
 
 	private int price;
 
-	@Lob private String content;
+	@Column(columnDefinition = "TEXT", length = 2000)
+	private String content;
 
 	private boolean isHidden;
 
