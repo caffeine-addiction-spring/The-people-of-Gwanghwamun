4fb8fdd0ed4508e1c33b390d984e00e3641be77e
Daae-Kim <wlsemrdl1212@naver.com>
2025-10-16 16:07:19 +0900

    feat : PaymentRepository 추가

---
+++ src/main/java/com/caffeine/gwanghwamun/domain/payment/repository/PaymentRepository.java
@@ -0,0 +1,20 @@
+package com.caffeine.gwanghwamun.domain.payment.repository;
+
+import com.caffeine.gwanghwamun.domain.payment.entity.Payment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+
+@RestController
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+
+    boolean existsByOrderId(UUID orderId);
+
+    Page<Payment> findByUserId(Long userId, Pageable pageable);
+
+    Page<Payment> findByStoreId(UUID storeId, Pageable pageable);
+}
