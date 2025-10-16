33315aa885e0b537be2a29f659d9b0d3c153d316
minju26 <mjalswn26@gmail.com>
2025-10-16 14:14:27 +0900

    chore: 스웨거 추가 설정
    - 서버 주소 추가
    - 인증 헤더 추가

--- src/main/java/com/caffeine/gwanghwamun/common/config/SwaggerConfig.java
+++ src/main/java/com/caffeine/gwanghwamun/common/config/SwaggerConfig.java
@@ -1,7 +1,12 @@
 package com.caffeine.gwanghwamun.common.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +15,39 @@ public class SwaggerConfig {
 
 	@Bean
 	public OpenAPI customOpenAPI() {
+
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList("accessToken");
+		Components components =
+				new Components()
+						.addSecuritySchemes(
+								"accessToken",
+								new SecurityScheme()
+										.name("accessToken")
+										.type(SecurityScheme.Type.HTTP)
+										.scheme("bearer")
+										.bearerFormat("JWT"));
+
 		return new OpenAPI()
-				.info(new Info().title("광화문의 민족 API").description("배달 주문 관리 플랫폼 API 문서").version("v1.0.0"));
+				.info(apiInfo())
+				.servers(servers())
+				.components(components)
+				.addSecurityItem(securityRequirement);
+	}
+
+	private Info apiInfo() {
+		return new Info().title("광화문의 민족 API").description("배달 주문 관리 플랫폼 API 문서").version("v1.0.0");
+	}
+
+	private List<Server> servers() {
+		Server local = new Server();
+		Server deploy = new Server();
+
+		local.setUrl("http://localhost:8081");
+		local.setDescription("백엔드 로컬");
+
+		deploy.setUrl("https://the-people-of-gwangh.kro.kr");
+		deploy.setDescription("백엔드 배포");
+
+		return List.of(local, deploy);
 	}
 }
