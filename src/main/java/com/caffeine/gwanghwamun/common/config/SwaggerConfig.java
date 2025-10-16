8e566e9ce05698f6a6ee085fa86d9bee93beb992
minju26 <mjalswn26@gmail.com>
2025-10-16 14:34:05 +0900

    chore: 로그인 API 내용 수동 추가

--- src/main/java/com/caffeine/gwanghwamun/common/config/SwaggerConfig.java
+++ src/main/java/com/caffeine/gwanghwamun/common/config/SwaggerConfig.java
@@ -1,12 +1,20 @@
 package com.caffeine.gwanghwamun.common.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.*;
+import io.swagger.v3.oas.models.headers.Header;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -31,7 +39,8 @@ public class SwaggerConfig {
 				.info(apiInfo())
 				.servers(servers())
 				.components(components)
-				.addSecurityItem(securityRequirement);
+				.addSecurityItem(securityRequirement)
+				.paths(customPaths());
 	}
 
 	private Info apiInfo() {
@@ -49,5 +58,40 @@ public class SwaggerConfig {
 		deploy.setDescription("백엔드 배포");
 
 		return List.of(local, deploy);
+	}
+
+	private Paths customPaths() {
+		Paths paths = new Paths();
+
+		PathItem loginPath = new PathItem()
+				.post(new Operation()
+						.tags(List.of("인증"))
+						.summary("로그인 API")
+						.description("이메일, 비밀번호를 통한 로그인")
+						.requestBody(new RequestBody()
+								.content(new Content()
+										.addMediaType("application/json",
+												new MediaType()
+														.schema(new Schema<>()
+																.type("object")
+																.addProperty("email", new Schema<>().type("string").example("user@example.com"))
+																.addProperty("password", new Schema<>().type("string").example("password123"))
+														)
+										)
+								)
+						)
+						.responses(new ApiResponses()
+								.addApiResponse("200",
+										new ApiResponse()
+												.description("로그인 성공")
+												.headers(Map.of("Authorization",
+														new Header().description("JWT 토큰").schema(new Schema<>().type("string"))
+												))
+								)
+						)
+				);
+
+		paths.addPathItem("/v1/auth/login", loginPath);
+		return paths;
 	}
 }
