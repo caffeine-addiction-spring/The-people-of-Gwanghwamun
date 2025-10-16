2577f647c42795684d50e9bbd8e003b85f2fd3dd
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-10-16 10:25:33 +0900

    chore: 코드 포맷팅

--- src/main/java/com/caffeine/gwanghwamun/domain/menu/entity/Menu.java
+++ src/main/java/com/caffeine/gwanghwamun/domain/menu/entity/Menu.java
@@ -3,13 +3,12 @@ package com.caffeine.gwanghwamun.domain.menu.entity;
 import com.caffeine.gwanghwamun.domain.BaseEntity;
 import com.caffeine.gwanghwamun.domain.store.entity.Store;
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Entity
 @Table(name = "p_menu")
