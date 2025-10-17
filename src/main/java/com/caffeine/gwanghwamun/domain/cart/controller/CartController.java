7642f27fe1cf4ca7f9d272d426f1c1d8446e5e80
taeaeaeae <haenimhana@gmail.com>
2025-10-17 12:17:34 +0900

    refactor: 스웨거 Sammary 수정

--- src/main/java/com/caffeine/gwanghwamun/domain/cart/controller/CartController.java
+++ src/main/java/com/caffeine/gwanghwamun/domain/cart/controller/CartController.java
@@ -7,60 +7,61 @@ import com.caffeine.gwanghwamun.common.success.SuccessCode;
 import com.caffeine.gwanghwamun.domain.cart.dto.*;
 import com.caffeine.gwanghwamun.domain.cart.service.CartService;
 import io.swagger.v3.oas.annotations.Operation;
-import java.util.List;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/v1/cart")
 @RequiredArgsConstructor
 public class CartController {
 
-	private final CartService cartService;
+  private final CartService cartService;
 
-	@PreAuthorize("hasRole('CUSTOMER')")
-	@Operation(summary = "장바구니 담기", description = "메뉴를 장바구니에 담는다.")
-	@PostMapping
-	public ResponseEntity<ApiResponse<SaveCartResDTO>> saveCart(
-			@RequestBody SaveCartReqDTO req, @AuthenticationPrincipal UserDetailsImpl user) {
+  @PreAuthorize("hasRole('CUSTOMER')")
+  @Operation(summary = "장바구니 담기", description = "메뉴를 장바구니에 담는다.")
+  @PostMapping
+  public ResponseEntity<ApiResponse<SaveCartResDTO>> saveCart(
+      @RequestBody SaveCartReqDTO req, @AuthenticationPrincipal UserDetailsImpl user) {
 
-		SaveCartResDTO cart = cartService.saveCart(user.getUser().getUserId(), req);
-		return ResponseUtil.successResponse(SuccessCode.CART_SAVE_SUCCESS, cart);
-	}
+    SaveCartResDTO cart = cartService.saveCart(user.getUser().getUserId(), req);
+    return ResponseUtil.successResponse(SuccessCode.CART_SAVE_SUCCESS, cart);
+  }
 
-	@PreAuthorize("hasRole('CUSTOMER')")
-	@Operation(summary = "장바구니 조회", description = "장바구니 목록을 조회한다.")
-	@GetMapping
-	public ResponseEntity<ApiResponse<List<CartResDTO>>> findCartList(
-			@AuthenticationPrincipal UserDetailsImpl user) {
+  @PreAuthorize("hasRole('CUSTOMER')")
+  @Operation(summary = "장바구니 조회 API", description = "장바구니 목록을 조회한다.")
+  @GetMapping
+  public ResponseEntity<ApiResponse<List<CartResDTO>>> findCartList(
+      @AuthenticationPrincipal UserDetailsImpl user) {
 
-		List<CartResDTO> cartItems = cartService.findCartList(user.getUser());
-		return ResponseUtil.successResponse(SuccessCode.CART_LIST_SUCCESS, cartItems);
-	}
+    List<CartResDTO> cartItems = cartService.findCartList(user.getUser());
+    return ResponseUtil.successResponse(SuccessCode.CART_LIST_SUCCESS, cartItems);
+  }
 
-	@PreAuthorize("hasRole('CUSTOMER')")
-	@Operation(summary = "장바구니 항목 수정", description = "장바구니에 담긴 메뉴의 옵션과 수량을 수정한다.")
-	@PutMapping("/{cartId}")
-	public ResponseEntity<ApiResponse<CartUpdateResDTO>> updateCart(
-			@PathVariable("cartId") UUID cartId,
-			@RequestBody UpdateCartReqDTO cartUpdateReqDTO,
-			@AuthenticationPrincipal UserDetailsImpl user) {
+  @PreAuthorize("hasRole('CUSTOMER')")
+  @Operation(summary = "장바구니 항목 수정 API", description = "장바구니에 담긴 메뉴의 옵션과 수량을 수정한다.")
+  @PutMapping("/{cartId}")
+  public ResponseEntity<ApiResponse<CartUpdateResDTO>> updateCart(
+      @PathVariable("cartId") UUID cartId,
+      @RequestBody UpdateCartReqDTO cartUpdateReqDTO,
+      @AuthenticationPrincipal UserDetailsImpl user) {
 
-		CartUpdateResDTO updatedCart = cartService.updateCart(user.getUser(), cartId, cartUpdateReqDTO);
-		return ResponseUtil.successResponse(SuccessCode.CART_UPDATE_SUCCESS, updatedCart);
-	}
+    CartUpdateResDTO updatedCart = cartService.updateCart(user.getUser(), cartId, cartUpdateReqDTO);
+    return ResponseUtil.successResponse(SuccessCode.CART_UPDATE_SUCCESS, updatedCart);
+  }
 
-	@PreAuthorize("hasAnyRole('CUSTOMER', 'MASTER')")
-	@Operation(summary = "장바구니 항목 삭제", description = "장바구니에서 항목을 삭제한다.")
-	@DeleteMapping("/{cartId}")
-	public ResponseEntity<ApiResponse<Void>> deleteCart(
-			@PathVariable("cartId") UUID cartId, @AuthenticationPrincipal UserDetailsImpl user) {
+  @PreAuthorize("hasAnyRole('CUSTOMER', 'MASTER')")
+  @Operation(summary = "장바구니 항목 삭제 API", description = "장바구니에서 항목을 삭제한다.")
+  @DeleteMapping("/{cartId}")
+  public ResponseEntity<ApiResponse<Void>> deleteCart(
+      @PathVariable("cartId") UUID cartId, @AuthenticationPrincipal UserDetailsImpl user) {
 
-		cartService.deleteCart(user.getUser(), cartId);
-		return ResponseUtil.successResponse(SuccessCode.CART_DELETE_SUCCESS);
-	}
+    cartService.deleteCart(user.getUser(), cartId);
+    return ResponseUtil.successResponse(SuccessCode.CART_DELETE_SUCCESS);
+  }
 }
