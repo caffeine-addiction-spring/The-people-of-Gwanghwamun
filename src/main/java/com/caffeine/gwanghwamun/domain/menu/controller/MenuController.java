b9c814a8eedecfbb1b6519f28dfb7abc8f81d6f7
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-10-17 12:00:44 +0900

    chore: 코드 포맷팅

--- src/main/java/com/caffeine/gwanghwamun/domain/menu/controller/MenuController.java
+++ src/main/java/com/caffeine/gwanghwamun/domain/menu/controller/MenuController.java
@@ -13,6 +13,8 @@ import com.caffeine.gwanghwamun.domain.menu.dto.request.MenuVisibilityReqDTO;
 import com.caffeine.gwanghwamun.domain.menu.dto.response.MenuResDTO;
 import com.caffeine.gwanghwamun.domain.menu.service.MenuService;
 import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,9 +24,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/stores/{storeId}/menus")
