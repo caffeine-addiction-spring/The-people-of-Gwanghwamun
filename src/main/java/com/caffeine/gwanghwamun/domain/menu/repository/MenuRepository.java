1dde828dc18c299c0b3651c6bc731ab2ba1ff58a
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-10-17 14:04:50 +0900

    chore: 코드 포맷팅

--- src/main/java/com/caffeine/gwanghwamun/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/caffeine/gwanghwamun/domain/menu/repository/MenuRepository.java
@@ -3,15 +3,14 @@ package com.caffeine.gwanghwamun.domain.menu.repository;
 import com.caffeine.gwanghwamun.domain.menu.entity.Menu;
 import com.caffeine.gwanghwamun.domain.menu.entity.QMenu;
 import com.querydsl.core.BooleanBuilder;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
-import java.util.UUID;
 
 public interface MenuRepository extends JpaRepository<Menu, UUID>, QuerydslPredicateExecutor<Menu> {
 
