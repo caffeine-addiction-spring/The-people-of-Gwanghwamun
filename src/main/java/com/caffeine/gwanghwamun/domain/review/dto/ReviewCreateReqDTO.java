f955a502630d2bd06781af90975f00e74b03b301
Daae-Kim <wlsemrdl1212@naver.com>
2025-10-17 12:06:49 +0900

    refactor : 평점 validation 추가

--- src/main/java/com/caffeine/gwanghwamun/domain/review/dto/ReviewCreateReqDTO.java
+++ src/main/java/com/caffeine/gwanghwamun/domain/review/dto/ReviewCreateReqDTO.java
@@ -1,6 +1,12 @@
 package com.caffeine.gwanghwamun.domain.review.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record ReviewCreateReqDTO(@Size(max = 1000) String content, @NotNull Long rating) {}
+public record ReviewCreateReqDTO(@Size(max = 1000) String content,
+                                 @NotNull(message = "평점은 필수입니다.")
+                                 @Min(value = 1, message = "평점은 최소 1점입니다")
+                                 @Max(value = 5, message = "평점은 최대 5점입니다")
+                                 Long rating) {}
